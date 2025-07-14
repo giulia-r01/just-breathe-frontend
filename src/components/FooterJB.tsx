eaa8098f556dbf76a2a1fb2b9eb45533092a4c31
Giulia Rizzo <girzzo@gmail.com>
2025-07-14 17:34:37 +0200

    aggiunti recapitio e social al footer

--- src/components/FooterJB.tsx
+++ src/components/FooterJB.tsx
@@ -1,8 +1,53 @@
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
+
 const FooterJB = function () {
   return (
-    <footer className="text-center small mynav py-2">
+    <footer className="text-center mynav py-2">
       <p className="mb-0 text-white">
-        🌿 Just Breathe - <span>{new Date().getFullYear()}</span>
+        🌿 Just Breathe - &copy; <span>{new Date().getFullYear()} 🌿</span>
+      </p>
+      <p className="mb-0 text-white">
+        Seguici:{" "}
+        <a
+          title="Apri su Instagram"
+          className="mx-2"
+          href="https://www.instagram.com/giuliarizzo00/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Profilo instagram di Just Breathe - Collegamento a sito esterno - Apertura in nuova scheda"
+        >
+          <FaInstagram aria-hidden="true" />
+        </a>{" "}
+        <a
+          title="Apri su LinkedIn"
+          className="mx-2"
+          href="https://www.linkedin.com/in/giulia-rizzo-4782bb102/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Profilo LinkedIn di Just Breathe - Collegamento a sito esterno - Apertura in nuova scheda"
+        >
+          <FaLinkedin aria-hidden="true" />
+        </a>{" "}
+        <a
+          title="Apri su GitHub"
+          className="mx-2"
+          href="https://github.com/giulia-r01"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Profilo GitHub di Just Breathe - Collegamento a sito esterno - Apertura in nuova scheda"
+        >
+          <FaGithub aria-hidden="true" />
+        </a>
+      </p>
+      <p className="mb-0 text-white">
+        Assistenza: Hai bisogno di aiuto? Scrivici una{" "}
+        <a
+          href="mailto:girzzo@gmail.com?subject=Richiesta%20di%20assistenza"
+          className="text-white"
+          aria-label="mail - Invia una mail, oggetto: Richiesta di assistenza"
+        >
+          mail
+        </a>
       </p>
     </footer>
   )
