40a8dea2b976dface7eb2aa354a1ffb6007a6e82
Giulia Rizzo <girzzo@gmail.com>
2025-07-17 09:29:00 +0200

    Update ToDoList.tsx

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -56,6 +56,17 @@ const ToDoList = () => {
     }
   }
 
+  function formatDateForInput(dateString: string) {
+    if (!dateString) return ""
+    const date = new Date(dateString)
+
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, "0")
+    const day = String(date.getDate()).padStart(2, "0")
+
+    return `${year}-${month}-${day}`
+  }
+
   useEffect(() => {
     const fetchTasks = async () => {
       setLoading(true)
@@ -393,9 +404,7 @@ const ToDoList = () => {
                 name="dataCreazioneTask"
                 value={
                   currentTask
-                    ? new Date(currentTask.dataCreazioneTask)
-                        .toISOString()
-                        .slice(0, 10)
+                    ? formatDateForInput(currentTask.dataCreazioneTask)
                     : ""
                 }
                 onChange={handleChange}
